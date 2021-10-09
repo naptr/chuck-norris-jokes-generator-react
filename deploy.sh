9b57e3e5ae0f0f2d6c98830b780dc4ec822c5e36
Putra Krihshna <krishna.putra332@gmail.com>
2021-10-09 11:27:33 +0700

    add letter deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git init
 git add -A
 git commit -m "deploy"
 
-# deploy to https://ziunadev.github.io/<Repo>
+# deploy to https://ziunadev.github.io/<REPO>
 git push -f git@github.com:ziunadev/chuck-norris-jokes-generator-react.git main:gh-pages
 
 cd -
